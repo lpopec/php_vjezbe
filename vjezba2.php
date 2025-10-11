a78f8e5419121c6f33b72591a0b5a91c6ddb2f3c
lpopec <lovromijopopec@gmail.com>
2025-10-11 11:14:32 +0200

    Create vjezba2.php

---
+++ vjezba2.php
@@ -0,0 +1,23 @@
+<?php
+    $title = "Da Vincijev kod";
+    $link = "https://hr.wikipedia.org/wiki/Da_Vincijev_kod";
+    /*PHP vježba 1*/
+    echo '  <!DOCTYPE html>
+            <html lang="hr">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta name="description" content="Free Web tutorials">
+                <meta name="keywords" content="'. $title .'">
+                <meta name="author" content="Lovro-Mijo Popec">
+                <title>'. $title .'</title>
+            </head>
+            <body>
+                <h1>'. $title .'</h1>
+                <p>'. $title .' je kriminalistički triler američkog pisca Dana Browna.</p>
+                <a href="'. $link .'" target="_blank">'. $link .'</a>
+            </body>
+            <footer>
+            </footer>
+    ';
+?>
