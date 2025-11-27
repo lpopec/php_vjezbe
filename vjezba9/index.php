614ab4ce1d729a936b1d565e837619cc52081ce6
lpopec <92295390+lpopec@users.noreply.github.com>
2025-11-27 18:34:42 +0100

    Update index.php

--- vjezba9/index.php
+++ vjezba9/index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Odabir automobila</title>
+    <title>Ducan</title>
     <style>
         body {
             font-family: Arial, sans-serif;
